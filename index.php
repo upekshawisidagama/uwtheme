c4629f4c1e7b2aaaf300464123d4d10662822aae
upeksha <https://github.com/upekshawisidaga>
2013-01-12 23:29:34 +0530

    added files

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php 
+// get theme header.
+get_header();
 
+// determine if we have posts remaining to be displayed.
 if ( have_posts() ) :
 
+    // while we have posts we loop through them.
     while ( have_posts() ) : 
     
-        the_post();
-        the_title();
+        the_post(); // advance onto the next post, and set the global $post variable
+
+        // display the title and the content of current post.
+        echo '<h1>' . get_the_title() . '</h1>';
         the_content();
         
     endwhile;
 
 endif;
+
+// get theme footer.
+get_footer();
 
 ?>
